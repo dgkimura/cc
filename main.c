a101865505b7f4d6eb597510bac54cc0909564e6
David Kimura <dkimura@pivotal.io>
2019-02-15 22:33:45 -0800

    Add file input to cc

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
+#include <stdio.h>
+
 #include "parser.h"
 
+extern FILE *yyin;
 extern int yyparse();
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc > 0)
+    {
+        FILE *f = fopen(argv[1], "r");
+        yyin = f;
+    }
+
     yyparse();
     return 0;
 }
